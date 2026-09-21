8f745dea9b08856f9028c5c1a0a67a3407d75bd7
Pipeloluwa <pipeloluwa14@gmail.com>
2026-09-21 20:10:14 +0100

    fix: revert encryption fetch url in sandbox script

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.onsteroid.com/api/v1',
+  apiUrl: 'https://user.onsteroid.com/api/v1',
   proxyUrl: 'https://proxy.onsteroid.com/api/v1/Proxy',
   proxyFallbackUrl: 'https://proxy.onsteroid.com/api/v1/Proxy',
   encryptUrl: 'https://proxy.onsteroid.com/api/v1/auth/encrypt'
